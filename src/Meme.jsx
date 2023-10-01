117ccd682c88f155a52f20d614a3f440a7d3e079
MatRJ08 <matrj08@gmail.com>
2023-10-01 15:34:12 -0600

    Meme topText and bottomText added
    It was added the bottomText and topText properties to State and both are now displayed in the generated meme

--- src/Meme.jsx
+++ src/Meme.jsx
@@ -1,26 +1,62 @@
-import React from "react"
+import React, { useState } from "react"
 import memesData from "./memesData"
 
-
 export default function Meme(){
-    const [memeImage,  setMemeImage] = React.useState("a")
+
+    const [meme, setMeme] = React.useState({
+        topText: "", 
+        bottomText: "", 
+        randomImage: "http://i.imgflip.com/1bij.jpg"
+    })
+    
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
     
     function getMemeImage(){
-        const memesArray = memesData.data.memes
+        const memesArray = allMemeImages.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
-        setMemeImage(memesArray[randomNumber].url)
-        // memesArray[randomNumber].url  <-- this line is incomplete!
-        
+        setMeme(pastMeme => ({
+            ...pastMeme, randomImage: memesArray[randomNumber].url
+        }))        
     }
+    function handleChange(event){
+        console.log(event.target)
+        const {name, value} = event.target
+        event.preventDefault()
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
+    }
+    
     return(<div className="meme">
         <div>
             <section className ="meme-textValues">
-                <input type="text" className = "meme-textValue" name="text-1" defaultValue="Shut Up" />
-                <input type="text" className = "meme-textValue" name="text-2" defaultValue="And take my money"/>
+                <input 
+                type="text"
+                placeholder="Top text"
+                className="meme-textValue"
+                name = "topText"
+                value = {meme.topText}
+                onChange = {handleChange}
+                />
+                <input 
+                    type="text"
+                    placeholder="Bottom text"
+                    className="meme-textValue"
+                    name = "bottomText"
+                    value = {meme.bottomText}
+                    onChange = {handleChange}
+                />
             </section>
             <button className = "meme-submit" onClick={getMemeImage}>Get a new meme image</button>
         </div>
-        <img src={memeImage} className="meme-Image" />
+        <div className="meme">
+            <img src={meme.randomImage} className="meme-Image" />
+            <h2 className="meme-text top">{meme.topText}</h2>
+            <h2 className="meme-text bottom">{meme.bottomText}</h2> 
+        </div>
     
     </div>)
+
+    
 }
